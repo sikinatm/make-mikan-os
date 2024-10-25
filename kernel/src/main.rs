3d4477796662e069a7962e2e9ecdfa0981406378
sikinatm <tomoaki.shikina@gmail.com>
2024-10-26 01:55:56 +0900

    feat: 適当な模様を画面に描く

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,15 +4,19 @@
 use core::arch::asm;
 
 #[no_mangle]
-pub extern "C" fn KernelMain() -> ! {
-    
-    loop {
+pub extern "C" fn KernelMain(frame_buffer_base: u64, frame_buffer_size: u64) {
+    let frame_buffer = frame_buffer_base as *mut u8;
+
+    for i in 0..frame_buffer_size {
         unsafe {
-            asm!("hlt");
+            *frame_buffer.offset(i as isize) = (i % 256) as u8;
         }
     }
-}
 
+    loop {
+        unsafe { asm!("hlt") };
+    }
+}
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
